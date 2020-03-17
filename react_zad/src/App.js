b649958abb2c1f3e9850fb5ecab8d199dde89a7d
kmihelec <kmihelec@hotmail.com>
2020-03-17 19:44:18 +0100

    fetch, list songs

--- react_zad/src/App.js
+++ react_zad/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            loading:false,
+            error:null,
+            tracks:[],
+            modal:false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({loading:true});
+        this.fetchData()
+    }
+
+     fetchData=async ()=>{
+         let topList;
+         let tracks =[];
+         try{
+             const data = await fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart');
+             topList = await data.json();
+             tracks = topList.tracks.data.map(track=>track)
+             this.setState(state=>({loading:false, tracks:state.tracks.concat(tracks)}))
+         }catch (e) {
+             this.setState({loading:false,error:'Unable to fetch tracks!'})
+             console.log(e);
+
+         }
+    };
+
+
+
+  render() {
+      if(this.state.loading) return <h2>Loading...</h2>;
+      if(this.state.error)return <h2>{this.state.error}</h2>;
+      return (
+          <div className={style.App} >
+              {this.state.tracks.map(track=>{
+                  return(<div key={track.id} className={style.track}>
+                        <div>{track.title}</div>
+                        <div>{track.duration}s</div>
+                  </div>)
+              })}
+          </div>
+      );
+  }
+
+
 }
 
 export default App;
